3816c6b4158f203725b0964d92670f3ac333a732
Megharth <m.c.lakhataria@gmail.com>
2019-10-15 19:00:07 -0400

    update game.jsx | add disks to the board

--- assets/js/game.jsx
+++ assets/js/game.jsx
@@ -12,15 +12,48 @@ class Checkers extends React.Component {
         
         // Setting up color for each tile in the board.
         let board = Array(64).fill(null).map((el, index) => {
-            if(index % 2 === 0)
-                return {
-                    colorTile: "white"
-                }
-            else
-                return {
-                    colorTile: "red"
-                }
+            return {
+                position: index,
+            }
         })
+
+        let blacks = Array(12).fill(null)
+        let whites = Array(12).fill(null)
+
+        let count = 0;
+
+        // Assign each disk an initial position and assign that disk to
+        // the corresponding tile in the board.
+        for(let i=0; i<3; i++) {
+            for(let j=0; j<8; j+=2) {
+                if(i%2 == 0) {
+                    blacks[count] = {
+                        color: "black",
+                        position: 40 + 8*i + j
+                    }
+                    whites[count] = {
+                        color: "white",
+                        position: 8*i + j
+                    }
+                    board[8*i + j]["disk"] = whites[count]
+                    board[40 + 8*i + j]["disk"] = blacks[count]
+                    count++
+                }
+                else {
+                    blacks[count] = {
+                        color: "black",
+                        position: 41 + 8*i + j
+                    }
+                    whites[count] = {
+                        color: "white",
+                        position: 8*i + j + 1
+                    }
+                    board[1 + 8*i + j]["disk"] = whites[count]
+                    board[41 + 8*i + j]["disk"] = blacks[count]
+                    count++
+                }
+            }
+        }
 
         let msgs = [
             "Message1", 
@@ -44,8 +77,10 @@ class Checkers extends React.Component {
         this.state = {
             board: board,
             message: msgs.map((msg, index) => {
-                return <div className="message" key={index}>{msg}</div>
-            }) 
+                return <div className="message" key={index}><div className="message-text">{msg}</div></div>
+            }),
+            whites: whites,
+            blacks: blacks 
         }
 
         console.log(this.state)
@@ -56,22 +91,37 @@ class Checkers extends React.Component {
         let row = []
         for (let i = 0; i < 8; i++) {
             let col = []
-            for (let j = 0; j < 8; j++)
-                col.push(this.createTile(i+j))
+            for (let j = 0; j < 8; j++) {
+                let color = (i+j)%2 === 0 ? "white" : "red"
+                col.push(this.createTile((i*8 +j), color))
+
+            }
             row.push(<div className="board-row" key={i}>{col}</div>)
         }
         return row
     }
 
     //To create a tile.
-    createTile(index) {
+    createTile(index, color) {
         return (
-            <div className="board-col" key={index}>
-                <Tile color={this.state.board[index].colorTile}/>
+            <div className="board-col" key={index} id={"tile-" + index}>
+                <Tile color={color} disk={this.state.board[index].disk}/>
             </div>
         )
     }
 
+    // A temp function to see the re-rendering of the board
+    demo() {
+        let board = this.state.board
+        let disk = board[0].disk
+        board[18].disk = null
+        disk.position = 27
+        board[27].disk = disk
+        console.log(board)
+        this.setState({
+            board
+        })
+    }
     render() {
         return (
             <div>
@@ -79,9 +129,10 @@ class Checkers extends React.Component {
                     {/* GAME BOARD */}
                     <div className="column">
                         <div className="board">{this.createBoard()}</div>
+
                         <div className="row action-row">
                         <div className="column">
-                            <button>Quit Game</button>
+                            <button onClick={() => {this.demo()}}>Quit Game</button>
                         </div>
                         <div className="column">
                             <button>Raise a draw</button>
@@ -109,13 +160,43 @@ class Checkers extends React.Component {
 // Component for tile.
 function Tile(props) {
     if(props.color == "red") {
+        if(props.disk) {
+            return (
+                <div className="tile-red">
+                    <Disk color={props.disk.color}/>
+                </div>
+            )
+        } else {
+            return (
+                <div className="tile-red"></div>
+            )
+        }
+    }
+    else {
+        if(props.disk) {
+            return (
+                <div className="tile-white">
+                    <Disk color={props.disk.color} />
+                </div>
+            )
+        } else {
+            return (
+                <div className="tile-white"></div>
+            )
+        }
+    }
+}
+
+// Component for disk.
+function Disk(props) {
+    if(props.color == "black") {
         return (
-            <div className="tile-red"></div>
+            <div className="black-disk"></div>
         )
     }
     else {
         return (
-            <div className="tile-white"></div>
+            <div className="white-disk"></div>
         )
     }
 }
