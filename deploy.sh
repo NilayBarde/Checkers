e261a1cc4f2bb09fc6b870ff14e770bd5327f39e
Megharth <m.c.lakhataria@gmail.com>
2019-10-17 15:16:20 -0400

    setup for deployment

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=4800
+export NODEBIN=`pwd`/assets/node_modules/.bin
+export PATH="$PATH:NODEBIN"
+export SECRET_KEY_BASE="pKbut1/sTFGFnaUPjKs5j5ZNF2SqKzll4sdbwsQYTIqLEQYbWZ70WX3/EUcOOXEf"
+echo "Building..."
+
+mkdir -p ~/.config
+mkdir -p priv/static
+
+mix deps.get
+mix compile
+(cd assets && npm install)
+(cd assets && webpack --mode production)
+mix phx.digest
+
+echo "generating release..."
+mix release
+
+echo "starting app..."
+
+_build/prod/rel/checkers_game/bin/checkers_game start
